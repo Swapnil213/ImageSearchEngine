f837ae840c5e2f1aa6d53f0f3f51a16b15e9c6ce
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-01-02 19:59:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMore = document.getElementById("show-more");
+
